ef85c32511e9b94798c20c826b0902e7ba76b79a
JacobTStaggs <staggs11@marshall.edu>
2017-08-16 15:00:44 -0700

    kadd

--- chat/index.js
+++ chat/index.js
@@ -3,7 +3,6 @@ var express = require('express');
 var app = express();
 var server = require('http').createServer(app);
 var io = require('../..')(server);
-
 var port = process.env.PORT || 3000;
 
 server.listen(port, function () {
